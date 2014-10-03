5816ba3472c881fecea4c57f1405b1ef7386c38d
RyanJuthan <x00088236@ittd.ie>
2014-10-03 11:37:39 +0100

    history

---
+++ history.sh
@@ -0,0 +1,239 @@
+    1  ls -ltr
+    2  sudo gedit /etc/cntlm.conf 
+    3  bg
+    4  cd
+    5  find . -name *.crt -print 2>/dev/null
+    6  cd Downloads/
+    7  ls
+    8  vi eduroam-linux-IoTT_Linux.sh 
+    9  cd
+   10  sudo gedit /etc/cntlm.conf 
+   11  sudo gedit /etc/cntlm.conf 
+   12  bg
+   13  cntlm -M
+   14  cntlm -H
+   15  cntlm -H
+   16  cntlm -H
+   17  cntlm -H
+   18  sudo service cntlm restart
+   19  cntlm -H
+   20  cntlm -H cntlm.conf
+   21  cntlm cntlm.conf -H
+   22  cntlm -H
+   23  cntlm -H www.sun.com
+   24  cntlm -H
+   25  cntlm -H
+   26  ls /etc
+   27  sudo mv /etc/cntlm.conf /etc/cntlm.conf.old
+   28  cntlm -H
+   29  ls
+   30  sudo apt-get remove cntlm
+   31  sudo apt-get install cntlm
+   32  sudo apt-get remove cntlm
+   33  sudo apt-get install cntlm
+   34  sudo service start cntlm
+   35  sudo apt-get install cntlm
+   36  sudo service start 
+   37  sudo service cntlm start
+   38  sudo service cntlm start -v
+   39  sudo apt-get remove cntlm
+   40  sudo apt-get install cntlm
+   41  sudo apt-get remove cntlm
+   42  sudo apt-get install cntlm
+   43  sudo apt-get remove cntlm
+   44  find / -name cntlm.conf -print
+   45  find / -name cntlm.conf -print 2>/dev/nul
+   46  find / -name cntlm.conf -print 2>/dev/null
+   47  sudo apt-get remove cntlm
+   48  sudo apt-get install cntlm
+   49  sudo apt-get install cntlm
+   50  ls /etc/cntlm.conf
+   51  sudo apt-get purge cntlm
+   52  sudo apt-get install cntlm
+   53  find / -name cntlm.conf -print 2>/dev/null
+   54  cntlm -H
+   55  sudo vi /etc/cntlm.conf
+   56  cntlm -H
+   57  fg
+   58  sudo service cntlm restart
+   59  vi eduroam-linux-IoTT_Linux.sh 
+   60  sudo vi /etc/cntlm.conf
+   61  cntlm -M
+   62  cntlm -H -c /etc/cntlm.conf
+   63  cntlm -c /etc/cntlm.conf -H
+   64  cntlm -H
+   65  cntlm -c /etc/cntlm.conf -H
+   66  cntlm -c /etc/cntlm.conf 
+   67  cntlm -H -u jburns@stf
+   68  sudo vi /etc/cntlm.conf
+   69  sudo service cntlm restart
+   70  sudo vi /etc/cntlm.conf
+   71  sudo service cntlm restart
+   72  sudo service cntlm restart
+   73  sudo service cntlm restart
+   74  ls -ltr
+   75  rm *.cer
+   76  ls
+   77  chef
+   78  ls /opt
+   79  /opt/chef/bin/chef-solo
+   80  sudo shutdown -r now
+   81  ls -ltr
+   82  vi .profile 
+   83  export http_proxy=http://localhost:3128
+   84  sudo -E apt-get install git
+   85  git config --global user.email "<x00088236@ittd.ie>"
+   86  git config --global user.name "<RyanJuthan>"
+   87  git clone https://github.com/jzburns/EAA.lab.1.git
+   88  git clone https://github.com/jzburns/EAA.Week2.git
+   89  git clone https://github.com/RyanJuthan/EAA.Week2.git
+   90  cd EAA.Week2/
+   91  mkdir week 2
+   92  touch script.sh
+   93  cd ..
+   94  git add -A
+   95  git add -A
+   96  git add -A
+   97  mkdir EAA.lab
+   98  cd EAA.lab
+   99  mkdir week2
+  100  cd week2
+  101  touch script.sh
+  102  touch README.md
+  103  git init
+  104  gedit 
+  105  gedit readme.md
+  106  git clone https://github.com/RyanJuthan/EAA.Lab.git
+  107  cd ..
+  108  cd
+  109  mkdir EAA.Lab
+  110  touch README.md
+  111  git init
+  112  git add README.md
+  113  git commit -m "first commit"
+  114  git remote add origin https://github.com/RyanJuthan/EAA.Lab.git
+  115  git push -u origin master
+  116  cd EAA.Lab
+  117  mkdir week3
+  118  cd week3
+  119  gedit CPUCount.sh
+  120  gedit CPUCount.sh
+  121  git add -A
+  122  cd ..
+  123  cd ..
+  124  cd EAA.Lab
+  125  cd week3
+  126  cd ..
+  127  cd ..
+  128  git remote add origin https://github.com/RyanJuthan/EAA.Lab.git
+  129  git clone https://github.com/RyanJuthan/EAA.Lab.git
+  130  cd EAA.Lab
+  131  cd week3
+  132  cd..
+  133  cd ..
+  134  git add -A
+  135  git init
+  136  git add -A
+  137  git commit -a
+  138  got remote add origin https://github.com/RyanJuthan/EAA.Lab
+  139  https://github.com/RyanJuthan/EAA.Lab.git
+  140  git remote add origin https://github.com/RyanJuthan/EAA.Lab
+  141  git push -u origin master
+  142   git clone https://github.com/RyanJuthan/EAA.Lab.git
+  143  git init
+  144  git add -A
+  145  git commit -a
+  146  git remote add origin https://github.com/RyanJuthan/EAA.lab.git
+  147  git push -u origin master
+  148  cd ..
+  149  git add -A
+  150  git commit -a
+  151  git push -u origin master
+  152  Week 2 Lab (a): configuring github
+  153  Open a terminal and run:
+  154  export GIT_SSL_NO_VERIFY=1
+  155  export http_proxy=http://localhost:3128
+  156  sudo -E apt-get install git
+  157  git config --global user.email "<x00088236@ittd.ie>"
+  158  git config --global user.name "<RyanJuthan>"
+  159  cd EAA.Lab
+  160  ls
+  161  cd week3
+  162  ls
+  163  git add -A
+  164  cd ..
+  165  git add -A
+  166  git commit -a
+  167  cd week3
+  168  git add -A
+  169  git commit -a
+  170   cd ,,
+  171  cd ..
+  172  cd ..
+  173  git clone https://github.com/RyanJuthan/EAA.Lab.git
+  174  cd EAA.Lab
+  175  ls
+  176  git add -a 
+  177  git add -A
+  178  git commit -a
+  179  git remote add https://github.com/RyanJuthan/EAA.Lab.git
+  180  git remote add origin https://github.com/RyanJuthan/EAA.Lab.git
+  181  git push -u origin master
+  182  cd ..
+  183  git pull https://github.com/RyanJuthan/EAA.Lab.git
+  184  y
+  185  cd -
+  186  ls
+  187  git commit -a
+  188  git add -A
+  189  git commit -a
+  190  ls
+  191  ls week3/
+  192  cd ..
+  193  cd -
+  194  history | grep push
+  195  git push -u origin master
+  196  history
+  197  git config --global user.email "x00088236@ittd.ie"
+  198  git config --global user.name "RyanJuthan"
+  199  git push -u origin master
+  200  dc ..
+  201  cd ..
+  202  ls#
+  203  mv EAA.Lab EAA.Lab.old
+  204  git clone https://github.com/RyanJuthan/EAA.Lab.git
+  205  ls
+  206  cd EAA.Lab
+  207  ls
+  208  mkdir week3
+  209  cd week3/
+  210  cp ~/EAA.Lab.old/week3/CPUCount.sh .
+  211  cd ..
+  212  git add -A
+  213  git commit -a
+  214  git push -u origin master
+  215  cd ..
+  216  mkdir DEMO
+  217  cd DEMO
+  218  mkdir Week3
+  219  cd Week3
+  220  gedit Hello.sh
+  221  cd ..
+  222  touch README.md
+  223  git init
+  224  git add README.md
+  225  git commit -m "first commit"
+  226  git remote add origin https://github.com/RyanJuthan/DEMO.git
+  227  git push -u origin master
+  228  git clone https://github.com/RyanJuthan/DEMO.git
+  229  git add -A
+  230  git commit -a
+  231  git push origin master
+  232  cd ..
+  233  git clone https://github.com/RyanJuthan/EAA.FT.git
+  234  git commit -a
+  235  ld
+  236  ls
+  237  rm -rf EAA.Week2
+  238  ls
+  239  history > history.sh
