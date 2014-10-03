347d6915f2407499c72dc8ca439200df4dc4fc74
RyanJuthan <x00088236@ittd.ie>
2014-10-03 11:50:53 +0100

    changes

--- week3/CPUCount.sh
+++ week3/CPUCount.sh
@@ -1,1 +1,10 @@
-Grep processor/proc/cpuinfo | wc -l
+#!/bin/bash
+
+#: Title : Prcocessor 
+#: Date : 03/10/2014
+#: Author : Ryan Juthan 
+#: Version :1
+#: Description : counts the numbers of processors 
+#: Options :
+
+grep processor/proc/cpuinfo | wc -l
